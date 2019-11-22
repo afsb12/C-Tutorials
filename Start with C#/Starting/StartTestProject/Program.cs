232b78af6cefb1b76e86f380c2627ac266f74e6c
alisson ferreira <alissonfsb14@hotmail.com>
2019-11-22 00:02:06 -0300

    First Commit

---
+++ Start with C#/Starting/StartTestProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StartTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Best Friend");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
